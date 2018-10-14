e356760867aeaa2b6bc46175a6f020feec60bb05
haog <haoguo@unity3d.com>
2018-10-14 19:15:16 +0800

    dart implementation

--- android/src/main/java/me/haog/mmkv/MmkvPlugin.java
+++ android/src/main/java/me/haog/mmkv/MmkvPlugin.java
@@ -1,6 +1,7 @@
 package me.haog.mmkv;
 
 import android.content.Context;
+import android.util.Base64;
 
 import com.tencent.mmkv.MMKV;
 
@@ -21,11 +22,14 @@ public final class MmkvPlugin implements MethodCallHandler {
   private MmkvPlugin(Context context) {
     rootDir = MMKV.initialize(context);
     instances = new HashMap<String, MMKV>();
+    defaultInstance = MMKV.defaultMMKV();
   }
 
   private final String rootDir;
 
   private final Map<String, MMKV> instances;
+
+  private final MMKV defaultInstance;
 
   /**
    * Plugin registration.
@@ -37,86 +41,70 @@ public final class MmkvPlugin implements MethodCallHandler {
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if ("putBoolean".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .encode(call.<String>argument("key"), call.<Boolean>argument("value"));
-    } else if ("putInt".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .encode(call.<String>argument("key"), call.<Integer>argument("value"));
-    } else if ("putLong".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .encode(call.<String>argument("key"), call.<Long>argument("value"));
-    } else if ("putString".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .encode(call.<String>argument("key"), call.<String>argument("value"));
-    } else if ("putDouble".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .encode(call.<String>argument("key"), call.<Double>argument("value"));
-    } else if ("putBytes".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .encode(call.<String>argument("key"), call.<byte[]>argument("value"));
-    } else if ("getBoolean".equals(call.method)) {
-      boolean value = instances
-          .get(call.argument("id"))
-          .decodeBool(
-              call.<String>argument("key"),
-              call.hasArgument("defaultValue") ? call.<Boolean>argument("defaultValue") : false);
-    } else if ("getInt".equals(call.method)) {
-      int value = instances
-          .get(call.argument("id"))
-          .decodeInt(
-              call.<String>argument("key"),
-              call.hasArgument("defaultValue") ? call.<Integer>argument("defaultValue") : 0);
-    } else if ("getLong".equals(call.method)) {
-      long value = instances
-          .get(call.argument("id"))
-          .decodeLong(
-              call.<String>argument("key"),
-              call.hasArgument("defaultValue") ? call.<Long>argument("defaultValue") : 0L);
-    } else if ("getString".equals(call.method)) {
-      String value = instances
-          .get(call.argument("id"))
-          .decodeString(
-              call.<String>argument("key"),
-              call.hasArgument("defaultValue") ? call.<String>argument("defaultValue") : null);
-    } else if ("getDouble".equals(call.method)) {
-      double value = instances
-          .get(call.argument("id"))
-          .decodeDouble(
-              call.<String>argument("key"),
-              call.hasArgument("defaultValue") ? call.<Double>argument("defaultValue") : 0.0D);
-    } else if ("getBytes".equals(call.method)) {
-      byte[] value = instances
-          .get(call.argument("id"))
-          .decodeBytes(call.<String>argument("key"));
-    } else if ("containsKey".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .containsKey(call.<String>argument("key"));
-    } else if ("remove".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .removeValueForKey(call.<String>argument("key"));
-    } else if ("clear".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .clearAll();
-    } else if ("keys".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .allKeys();
-    } else if ("count".equals(call.method)) {
-      instances
-          .get(call.argument("id"))
-          .count();
+    if ("default".equals(call.method)) {
+      result.success(null); // dummy because init in constructor.
+    } else if ("withId".equals(call.method)) {
+      String id = call.argument("id");
+      instances.put(id, MMKV.mmkvWithID(id));
+    } else if ("withCryptKey".equals(call.method)) {
+      String id = call.argument("id");
+      byte[] cryptKey = call.argument("cryptKey");
+      String cryptKeySz = Base64.encodeToString(cryptKey, Base64.DEFAULT);
+      instances.put(id, MMKV.mmkvWithID(id, MMKV.SINGLE_PROCESS_MODE, cryptKeySz));
     } else {
-      result.notImplemented();
+      MMKV inst;
+      if (call.argument("default")) {
+        inst = defaultInstance;
+      } else {
+        inst = instances.get(call.argument("id"));
+      }
+      if ("putBoolean".equals(call.method)) {
+        inst.encode(call.<String>argument("key"), call.<Boolean>argument("value"));
+      } else if ("putInt".equals(call.method)) {
+        inst.encode(call.<String>argument("key"), call.<Integer>argument("value"));
+      } else if ("putLong".equals(call.method)) {
+        inst.encode(call.<String>argument("key"), call.<Long>argument("value"));
+      } else if ("putString".equals(call.method)) {
+        inst.encode(call.<String>argument("key"), call.<String>argument("value"));
+      } else if ("putDouble".equals(call.method)) {
+        inst.encode(call.<String>argument("key"), call.<Double>argument("value"));
+      } else if ("putBytes".equals(call.method)) {
+        inst.encode(call.<String>argument("key"), call.<byte[]>argument("value"));
+      } else if ("getBoolean".equals(call.method)) {
+        boolean value = inst.decodeBool(
+            call.<String>argument("key"),
+            call.hasArgument("defaultValue") ? call.<Boolean>argument("defaultValue") : false);
+      } else if ("getInt".equals(call.method)) {
+        int value = inst.decodeInt(
+            call.<String>argument("key"),
+            call.hasArgument("defaultValue") ? call.<Integer>argument("defaultValue") : 0);
+      } else if ("getLong".equals(call.method)) {
+        long value = inst.decodeLong(
+            call.<String>argument("key"),
+            call.hasArgument("defaultValue") ? call.<Long>argument("defaultValue") : 0L);
+      } else if ("getString".equals(call.method)) {
+        String value = inst.decodeString(
+            call.<String>argument("key"),
+            call.hasArgument("defaultValue") ? call.<String>argument("defaultValue") : null);
+      } else if ("getDouble".equals(call.method)) {
+        double value = inst.decodeDouble(
+            call.<String>argument("key"),
+            call.hasArgument("defaultValue") ? call.<Double>argument("defaultValue") : 0.0D);
+      } else if ("getBytes".equals(call.method)) {
+        byte[] value = inst.decodeBytes(call.<String>argument("key"));
+      } else if ("containsKey".equals(call.method)) {
+        inst.containsKey(call.<String>argument("key"));
+      } else if ("remove".equals(call.method)) {
+        inst.removeValueForKey(call.<String>argument("key"));
+      } else if ("clear".equals(call.method)) {
+        inst.clearAll();
+      } else if ("keys".equals(call.method)) {
+        inst.allKeys();
+      } else if ("count".equals(call.method)) {
+        inst.count();
+      } else {
+        result.notImplemented();
+      }
     }
   }
 }
--- lib/mmkv.dart
+++ lib/mmkv.dart
@@ -1,14 +1,75 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
 class Mmkv {
-  static const MethodChannel _channel =
-      const MethodChannel('mmkv');
+  static const MethodChannel _channel = const MethodChannel('mmkv');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    final a = {};
-    return version;
+  Mmkv._(this.isDefault, this.id);
+
+  final bool isDefault;
+  final String id;
+
+  static Future<Mmkv> defaultInstance() {
+    Completer<Mmkv> completer = Completer();
+    _channel.invokeMethod('default').then((value) => completer.complete(Mmkv._(true, null))).catchError((error) => completer.completeError(error));
+    return completer.future;
   }
+
+  static Future<Mmkv> withId(String id) {
+    Completer<Mmkv> completer = Completer();
+    _channel.invokeMethod('withId', {'id': id}).then((value) => completer.complete(Mmkv._(false, id))).catchError((error) => completer.completeError(error));
+    return completer.future;
+  }
+
+  static Future<Mmkv> withCryptKey(String id, Uint8List cryptKey) {
+    Completer<Mmkv> completer = Completer();
+    _channel
+        .invokeMethod('withCryptKey', {'id': id, 'cryptKey': cryptKey})
+        .then((value) => completer.complete(Mmkv._(false, id)))
+        .catchError((error) => completer.completeError(error));
+    return completer.future;
+  }
+
+  Future _invoke(String method, Map<String, dynamic> arguments) {
+    Map<String, dynamic> params = {};
+    if (this.isDefault) {
+      params['default'] = true;
+    } else {
+      params['id'] = this.id;
+    }
+    params.addAll(arguments);
+    return _channel.invokeMethod(method, params);
+  }
+
+  void putBoolean(String key, bool value) async => await _invoke('putBoolean', {'key': key, 'value': value});
+
+  void putInt(String key, int value) async => await _invoke('putLong', {'key': key, 'value': value});
+
+  void putString(String key, String value) async => await _invoke('putString', {'key': key, 'value': value});
+
+  void putBytes(String key, Uint8List value) async => await _invoke('putBytes', {'key': key, 'value': value});
+
+  void putDouble(String key, double value) async => await _invoke('putDouble', {'key': key, 'value': value});
+
+  Future<bool> getBoolean(String key) => _invoke('getBoolean', {'key': key});
+
+  Future<int> getInt(String key) => _invoke('getInt', {'key': key});
+
+  Future<String> getString(String key) => _invoke('getString', {'key': key});
+
+  Future<Uint8List> getBytes(String key) => _invoke('getBytes', {'key': key});
+
+  Future<double> getDouble(String key) => _invoke('getDouble', {'key': key});
+
+  Future<bool> containsKey(String key) => _invoke('containsKey', {'key': key});
+
+  Future<void> remove(String key) => _invoke('remove', {'key': key});
+
+  Future<void> clear() => _invoke('clear', {});
+
+  Future<List<String>> keys() => _invoke('keys', {});
+
+  Future<int> count() => _invoke('count', {});
 }
